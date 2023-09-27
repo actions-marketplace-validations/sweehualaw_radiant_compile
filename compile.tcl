41576d5ab8c26e81c4325edde64aacc979a3ec02
SweeHua <143031079+sweehualaw@users.noreply.github.com>
2023-09-27 15:05:15 -0500

    Create compile.tcl

---
+++ compile.tcl
@@ -0,0 +1,11 @@
+set RDF_PATH [lindex $argv 0]
+
+puts "RDF_FILE: ${RDF_PATH}"
+
+set IMPL impl_1
+prj_open "${RDF_PATH}"
+prj_run Synthesis -impl $IMPL
+prj_run Map -impl $IMPL
+prj_run PAR -impl $IMPL
+prj_run Export -impl $IMPL
+prj_close
